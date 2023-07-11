d3f8eb1478a2529d85a996cd6627d2baa5e64e7b
Fernanda-Marinho <fmarinho1918@gmail.com>
2023-07-11 18:39:20 -0300

    buscar termos nos codigos

---
+++ find.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+busca() { #busca a frase e imprime o caminho 
+    caminho=$1
+    frase=$2
+
+    if grep -q "$frase" "$caminho"; then
+        echo $caminho
+    fi
+}
+
+verificaDiretorio() { #ve diretorio e subdiretorio
+    diretorio=$1
+    frase=$2
+
+    for i in "$diretorio"/*; do
+        if [ -d "$i" ]; then #eh um diretorio
+            verificaDiretorio "$i" "$frase"
+        elif [ -f "$i" ]; then #eh um arquivo 
+            if [[ $i == *.js ]]; then
+                busca "$i" "$frase" #faz a busca da frase 
+            fi
+        fi
+    done
+}
+
+echo "frase:"
+read frase
+diretorioAtual=$(pwd)
+verificaDiretorio "$diretorioAtual" "$frase"
